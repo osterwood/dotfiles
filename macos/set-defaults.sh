e3242c208c45b2df51792576d7fb4c1d51d1477f
Chris Osterwood <osterwood@gmail.com>
2017-09-16 15:22:22 -0400

    Remove Atom, change casks, change default prefs

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -26,9 +26,21 @@ defaults write NSGlobalDomain KeyRepeat -int 1
 defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
 defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
 
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+
 # Run the screensaver if we're in the bottom-left hot corner.
 defaults write com.apple.dock wvous-bl-corner -int 5
 defaults write com.apple.dock wvous-bl-modifier -int 0
+defaults write com.apple.dock autohide-delay -float 0
+defaults write com.apple.dock autohide -bool true
+defaults write com.apple.dock mineffect -string "scale"
+defaults write com.apple.dock enable-spring-load-actions-on-all-items -bool true
+defaults write com.apple.dock launchanim -bool false
+
+# Set file type for screenshots and turn off shadows
+defaults write com.apple.screencapture type -string "png"
+defaults write com.apple.screencapture disable-shadow -bool true
 
 # Hide Safari's bookmark bar.
 defaults write com.apple.Safari ShowFavoritesBar -bool false
@@ -39,3 +51,17 @@ defaults write com.apple.Safari IncludeDevelopMenu -bool true
 defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
 defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
 defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
+
+# Expand save panel by default
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
+
+# Automatically quit printer app once the print jobs complete
+defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
+
+defaults write com.apple.Safari ShowFavoritesBar -bool false               # Hide bookmarks bar by default
+defaults write com.apple.Safari ShowSidebarInTopSites -bool false          # Hide sidebar in Top Sites
+
+defaults write NSGlobalDomain AppleEnableMenuBarTransparency -bool false   # Disable transparency in menu bars
+defaults write NSGlobalDomain AppleShowScrollBars -string "Always"         # Always show menu bars
+defaults write NSGlobalDomain NSWindowResizeTime -float 0.                 # Increase window resize speed for Cocoa applications"
